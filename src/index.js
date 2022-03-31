5993e8dd65e997a517a1cb33cacb327795482df5
Quentin <qdimanche@gmail.com>
2022-03-31 11:58:48 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import RootContainer from './containers/RootContainer';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <RootContainer />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
